8f37745399b69b0b32d695425959c65747b6ad4b
Alex Sheverdin <alex@skalelabs.com>
2020-05-18 22:44:25 +0300

    SKALE-2095 Use skale session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,20 @@ from skale import Skale
 from skale.utils.web3_utils import init_web3
 from skale.wallets import Web3Wallet
 
-from tests.constants import ENDPOINT, ETH_PRIVATE_KEY, TEST_ABI_FILEPATH
+from tests.constants import ENDPOINT, ETH_PRIVATE_KEY, N_TEST_NODES, TEST_ABI_FILEPATH
+from tests.prepare_validator import create_dirs, create_set_of_nodes, get_active_ids
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def skale():
-    '''Returns a SKALE instance with provider from config'''
+    """Returns a SKALE instance with provider from config"""
     web3 = init_web3(ENDPOINT)
     wallet = Web3Wallet(ETH_PRIVATE_KEY, web3)
-    return Skale(ENDPOINT, TEST_ABI_FILEPATH, wallet)
+    skale = Skale(ENDPOINT, TEST_ABI_FILEPATH, wallet)
+
+    create_dirs()
+    ids = get_active_ids(skale)
+    print(f'Existing Node IDs = {ids}')
+    cur_node_id = max(ids) + 1 if len(ids) else 0
+    create_set_of_nodes(skale, cur_node_id, N_TEST_NODES)
+    return skale
